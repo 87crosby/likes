0191281b9fd12a1abd9a9853267942eea505a409
87crosby <71791452+87crosby@users.noreply.github.com>
2021-07-06 16:21:30 -0400

    Add files via upload

---
+++ Likes/script.js
@@ -0,0 +1,14 @@
+function likeCounter(){
+    var like = document.querySelector("#like1")
+    like.innerText = 1
+}
+
+function likeCounter2(){
+    var like = document.querySelector("#like2")
+    like.innerText = 1
+}
+
+function likeCounter3(){
+    var like = document.querySelector("#like3")
+    like.innerText = 1
+}
